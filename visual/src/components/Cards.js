9d99cdd92333b8b52e07bce5258af4d3ba68d5fc
Gustavo Santiago <gsantiago@telconet.ec>
2021-09-06 12:39:40 -0500

    Cards

--- visual/src/components/Cards.js
+++ visual/src/components/Cards.js
@@ -1,9 +1,51 @@
 const Cards = ({ meta }) => {
   return (
     <div className="card-container">
-      <div className="card-count">{meta.active}</div>
-      <div className="card-mean">{meta.mean}</div>
-      <div className="card-max">{meta.max}</div>
+      <div className="card-count">
+        <div style={{ fontSize: "0.9rem", color: "#666", margin: "1rem" }}>
+          Total de observaciones
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            marginTop: "1.5rem",
+            fontSize: "2rem",
+          }}
+        >
+          {meta.active}
+        </div>
+      </div>
+      <div className="card-mean">
+        <div style={{ fontSize: "0.9rem", color: "#666", margin: "1rem" }}>
+          Promedio
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            marginTop: "1.5rem",
+            fontSize: "2rem",
+          }}
+        >
+          {meta.mean} kWh
+        </div>
+      </div>
+      <div className="card-max">
+        <div style={{ fontSize: "0.9rem", color: "#666", margin: "1rem" }}>
+          Máximo
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            marginTop: "1.5rem",
+            fontSize: "2rem",
+          }}
+        >
+          {meta.max} kWh
+        </div>
+      </div>
     </div>
   );
 };
